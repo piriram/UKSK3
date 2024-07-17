6061ee34347b0255b1bcf5262fe721b37228d07d
piri <pyoram25@gmail.com>
2024-07-17 17:31:29 +0900

    Initial Commit

---
+++ UKSK3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UKSK3
+//
+//  Created by ram on 7/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
